2d0ca4b3a0d5ddaeb4e7ec9bc46b4723363f9b4a
n.kuklina <n.kuklina@corp.mail.ru>
2019-04-07 14:58:22 +0300

    Урок 6: основы JS

---
+++ urok_6/cicleNumbers.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let a, b;
+
+a = 15;
+b = 40;
+
+while(a <= b) {
+    console.log("My " + a);
+    a = a + 1;
+}
+
---
+++ urok_6/ifElseMaxNumber.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let a, b;
+
+a = 125;
+b = 75;
+
+if(a < b) {
+    // yes
+    console.log("Answer " + b);
+} else {
+    // no
+    console.log("Answer " + a);
+}
+
+console.log("My program works");
+
